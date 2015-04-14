e85eeb23b315e7dac74fd19efd70b41c5bf38991
Geoff Blosser <gblosser@ebsco.com>
2015-04-14 16:39:33 -0400

    lessons 1.1 and 1.2

--- src/Unit-1/DoThis/ConsoleReaderActor.cs
+++ src/Unit-1/DoThis/ConsoleReaderActor.cs
@@ -3,37 +3,92 @@ using Akka.Actor;
 
 namespace WinTail
 {
-    /// <summary>
-    /// Actor responsible for reading FROM the console. 
-    /// Also responsible for calling <see cref="ActorSystem.Shutdown"/>.
-    /// </summary>
-    class ConsoleReaderActor : UntypedActor
-    {
-        public const string ExitCommand = "exit";
-        private IActorRef _consoleWriterActor;
+	/// <summary>
+	/// Actor responsible for reading FROM the console. 
+	/// Also responsible for calling <see cref="ActorSystem.Shutdown"/>.
+	/// </summary>
+	class ConsoleReaderActor : UntypedActor
+	{
+		public const string StartCommand = "start";
+		public const string ExitCommand = "exit";
+		private IActorRef _consoleWriterActor;
 
-        public ConsoleReaderActor(IActorRef consoleWriterActor)
-        {
-            _consoleWriterActor = consoleWriterActor;
-        }
+		public ConsoleReaderActor(IActorRef consoleWriterActor)
+		{
+			_consoleWriterActor = consoleWriterActor;
+		}
 
-        protected override void OnReceive(object message)
-        {
-            var read = Console.ReadLine();
-            if (!string.IsNullOrEmpty(read) && String.Equals(read, ExitCommand, StringComparison.OrdinalIgnoreCase))
-            {
-                // shut down the system (acquire handle to system via
-                // this actors context)
-                Context.System.Shutdown();
-                return;
-            }
+		protected override void OnReceive(object message)
+		{
+			if (message.Equals(StartCommand))
+			{
+				DoPrintInstructions();
+			}
+			else if (message is Messages.InputError)
+			{
+				_consoleWriterActor.Tell((Messages.InputError)message);
+			}
 
-            // send input to the console writer to process and print
-            // YOU NEED TO FILL IN HERE
+			GetAndValidateInput();
+		}
 
-            // continue reading messages from the console
-            // YOU NEED TO FILL IN HERE
-        }
 
-    }
+		#region Internal methods
+
+		private void DoPrintInstructions()
+		{
+			Console.WriteLine("Write whatever you want into the console!");
+			Console.WriteLine("Some entries will pass validation, and some won't...\n\n");
+			Console.WriteLine("Type 'exit' to quit this application at any time.\n");
+		}
+
+		/// <summary>
+		/// Reads input from console, validates it, then signals appropriate response
+		/// (continue processing, error, success, etc.).
+		/// </summary>
+		private void GetAndValidateInput()
+		{
+			var message = Console.ReadLine();
+			if (string.IsNullOrEmpty(message))
+			{
+				// signal that the user needs to supply an input, as previously
+				// received input was blank
+				Self.Tell(new Messages.NullInputError("No input received."));
+			}
+			else if (String.Equals(message, ExitCommand, StringComparison.OrdinalIgnoreCase))
+			{
+				// shut down the entire actor system (allows the process to exit)
+				Context.System.Shutdown();
+			}
+			else
+			{
+				var valid = IsValid(message);
+				if (valid)
+				{
+					_consoleWriterActor.Tell(new Messages.InputSuccess("Thank you! Message was valid."));
+				}
+				else
+				{
+					Self.Tell(new Messages.ValidationError("Invalid: input had odd number of characters."));
+				}
+
+				// continue reading messages from console
+				Self.Tell(new Messages.ContinueProcessing());
+			}
+		}
+
+		/// <summary>
+		/// Validates <see cref="message"/>.
+		/// Currently says messages are valid if contain even number of characters.
+		/// </summary>
+		/// <param name="message"></param>
+		/// <returns></returns>
+		private static bool IsValid(string message)
+		{
+			var valid = message.Length % 2 == 0;
+			return valid;
+		}
+
+		#endregion Internal methods
+	}
 }
